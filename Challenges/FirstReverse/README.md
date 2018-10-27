10b1b62cc62414a043d7f6019efff499c38ccb6e
Thiago Prado <thiago@to-it.ca>
2018-10-27 15:25:49 -0400

    Adding first challenge

---
+++ Challenges/FirstReverse/README.md
@@ -0,0 +1,2 @@
+# Challenge
+Using the JavaScript language, have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH. 
---
+++ Challenges/FirstReverse/code.js
@@ -0,0 +1,44 @@
+function FirstReverse(str) { 
+  // Step 1. Use the split() method to return a new array
+  var splitString = str.split(""); // var splitString = "hello".split("");
+  // ["h", "e", "l", "l", "o"]
+
+  // Step 2. Use the reverse() method to reverse the new created array
+  var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+  // ["o", "l", "l", "e", "h"]
+
+  // Step 3. Use the join() method to join all elements of the array into a string
+  var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+  // "olleh"
+  
+  //Step 4. Return the reversed string
+  return joinArray; // "olleh"
+         
+}
+ 
+var str1 = "TorontoITSolutions";
+var str2 = "Toronto IT Soltuions"
+// keep this function call here 
+str1
+str2
+
+console.log(FirstReverse(str1));
+console.log(FirstReverse(str2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                            
+                            
+                            
+  
