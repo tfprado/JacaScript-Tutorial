49efd1245798834cb2b18abf3d05b1e39a8f5a41
Thiago Prado <thiago@to-it.ca>
2018-10-27 15:41:29 -0400

    Cleanup

--- Challenges/FirstReverse/code.js
+++ Challenges/FirstReverse/code.js
@@ -17,10 +17,10 @@ function FirstReverse(str) {
 }
  
 var str1 = "TorontoITSolutions";
-var str2 = "Toronto IT Soltuions"
+var str2 = "Toronto IT Soltuions";
 // keep this function call here 
-str1
-str2
+console.log(str1);
+console.log(str2);
 
 console.log(FirstReverse(str1));
 console.log(FirstReverse(str2));
